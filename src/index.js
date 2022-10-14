e8d17dc5baa736ffe3fbfdf8a12548fe908f06e9
Adi <adi92az@gmail.com>
2022-10-14 15:04:15 +0200

    Firebase setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("index JS")
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyABIsTCW2N2KR9KFsKRL8gundgAxjwXHrY",
+    authDomain: "fir-9-dojo-nn.firebaseapp.com",
+    projectId: "fir-9-dojo-nn",
+    storageBucket: "fir-9-dojo-nn.appspot.com",
+    messagingSenderId: "11294072029",
+    appId: "1:11294072029:web:b9a82e3dc5515d2ef9cf34"
+  };
+
+initializeApp(firebaseConfig);
