a361d9bb1d2710df0443a4673e19a32a53b7a985
Adi <adi92az@gmail.com>
2022-10-14 16:39:22 +0200

    Firestore setup; fetch data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import {
+  getFirestore, collection, getDocs
+} from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyABIsTCW2N2KR9KFsKRL8gundgAxjwXHrY",
@@ -9,4 +12,31 @@ const firebaseConfig = {
     appId: "1:11294072029:web:b9a82e3dc5515d2ef9cf34"
   };
 
+// init firebase app
 initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore()
+
+// collection ref
+const collectionRef = collection(db, "books")
+
+// get collection data
+getDocs(collectionRef)
+  .then( snapshot => {
+    // console.log(snapshot.docs)
+
+    const books = []
+
+    snapshot.docs.forEach((doc) => {
+      books.push({ 
+        ...doc.data(), 
+        id: doc.id
+      })
+    })
+
+    console.log(books)
+  })
+  .catch( err => {
+    console.log(err.message)
+  })
